7d4fa2cdf36d3ce15e72557bf90ff94e5bead1c0
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-10 19:28:37 +0800

    Added codes about Overview of React Components

---
+++ 01-Getting-Started/rn-starter-main/src/screens/ComponentsScreens.js
@@ -0,0 +1,17 @@
+// 2 important imports that are always declared in the start
+// React library is how different components work together
+import React from "react";
+// React-Native is used to show some content on the mobile device
+import { Text, StyleSheet } from "react-native";
+
+const ComponentsScreen = function () {
+  return <Text style={styles.textStyle}>This is the components screen</Text>;
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+  },
+});
+
+export default ComponentsScreen;
--- 01-Getting-Started/rn-starter-main/src/screens/HomeScreen.js
+++ 01-Getting-Started/rn-starter-main/src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Tit3</Text>;
+  return <Text style={styles.text}>Hello World</Text>;
 };
 
 const styles = StyleSheet.create({
