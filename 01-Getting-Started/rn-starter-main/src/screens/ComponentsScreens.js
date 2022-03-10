12514e9473393f3cc639c3ca1daa1f7e8f1fcf29
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-10 23:52:55 +0800

    Added notes about Rules of JSX

--- 01-Getting-Started/rn-starter-main/src/screens/ComponentsScreens.js
+++ 01-Getting-Started/rn-starter-main/src/screens/ComponentsScreens.js
@@ -2,11 +2,25 @@
 // React library is how different components work together
 import React from "react";
 // React-Native is used to show some content on the mobile device
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = function () {
   //{fontSize: 30} - will also work
-  return <Text style={styles.textStyle}>This is the components screen</Text>;
+
+  //3. Use JS variable inside JSX
+  const greeting = "Bye there!";
+  //4. Use JSX element inside a JSX block
+  const greeting2 = <Text>Hello to you!</Text>;
+
+  return (
+    //1. Text inside View
+    //2. style inside the Text block = 'props'
+    <View>
+      <Text style={styles.textStyle}>This is the components screen</Text>
+      {/* <Text>{greeting}</Text> */}
+      {greeting2}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
